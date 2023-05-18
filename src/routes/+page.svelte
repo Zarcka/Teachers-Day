418bfe03bd141ef35f015dc88ba92a1f2f954c86
Zarcka <zarckaston@gmail.com>
2023-05-18 08:00:03 +0400

    Created new branch to work from school

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { browser } from '$app/environment';
+
+  // Check if the app is running in the browser
+  if (browser) {
+    window.location.replace('https://zarcka.github.io/');
+  };
+</script>
+
+<h1>You will be redirected to the correct page</h1>
+<p>If you have not been automatically redirected, click on the link: <a href="https://zarcka.github.io/Teachers-Day/2023">2023</a> </p>
